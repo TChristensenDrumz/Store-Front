fbbd349b7f5d634f69f7a10a3f64a32b15da7133
nartexyu <70301421+nartexyu@users.noreply.github.com>
2021-02-04 14:21:40 -0800

    fixed merge conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -14,17 +14,30 @@ import EmailPassword from "./components/EmailPassword";
 import NameInput from "./components/NameInput";
 import Cart from "./pages/cart/Cart";
 import StoreLanding from "./pages/storeLanding/StoreLanding";
+<<<<<<< Updated upstream
 import AllProducts from "./pages/allProducts/allProducts"
 import EditStore from "./pages/editStore/EditStore";
 import ContactStore from "./pages/contactStore/ContactStore";
+=======
+
+// Import pages
+import SiteLanding from './pages/siteLanding/SiteLanding';
+
+>>>>>>> Stashed changes
 
 
 function App() {
   return (
     <Router>
+<<<<<<< Updated upstream
       <Header />
       <ContactStore />
       <Footer />
+=======
+      <Route exact path="/home">
+        <SiteLanding />
+      </Route>
+>>>>>>> Stashed changes
     </Router>
   );
 }
--- client/src/pages/siteLanding/SiteLanding.js
+++ client/src/pages/siteLanding/SiteLanding.js
@@ -1,24 +1,104 @@
+// Import dependencies
 import React from "react";
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Container from 'react-bootstrap/Container';
-import Preview from "../../components/Preview";
+
+// Import styling 
+import { Navbar, Nav, Button, Jumbotron, Container, Row, Col } from 'react-bootstrap';
+
+// Import components
+
 
 function SiteLanding() {
   return (
-    <Container fluid>
-      <Row className="justify-content-md-center">
-        <Col>
-          <Preview />
-        </Col>
-        <Col>
-          <Preview />
-        </Col>
-        <Col>
-          <Preview />
-        </Col>
-      </Row>
-    </Container>
+    <div> 
+      {/* Header */}
+      <Navbar collapseOnSelect expand="lg" bg="none" variant="light" className="p-5" fixed="top">
+          <Navbar.Brand className="ml-5"href="#home">Store Front</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="ml-auto mr-auto">
+                  <Nav.Link className="ml-4 mr-4" href="//">Shops</Nav.Link>
+                  <Nav.Link className="ml-4 mr-4" href="#about">Templates</Nav.Link>
+                  <Nav.Link className="ml-4 mr-4" href="//">About</Nav.Link>
+              </Nav>
+              <Nav className="mr-5">
+                  <Nav.Link href="//">Login</Nav.Link>
+                  <Button variant="outline-dark">Dark</Button>
+              </Nav>
+          </Navbar.Collapse>
+      </Navbar>
+
+      {/* First jumbotron */}
+      <Jumbotron fluid>
+        <Container style={{height:"90vh"}}>
+          <h1>Fluid jumbotron</h1>
+          <p>
+            This is a modified jumbotron that occupies the entire horizontal space of
+            its parent.
+          </p>
+        </Container>
+      </Jumbotron> 
+
+      {/* Second jumbotron */}
+      <Jumbotron fluid>
+        <Container style={{height:"30vh"}}>
+          <h1>Fluid jumbotron</h1>
+          <p>
+            This is a modified jumbotron that occupies the entire horizontal space of
+            its parent.
+          </p>
+        </Container>
+      </Jumbotron>
+
+      {/* Third jumbotron */}
+      <Jumbotron fluid>
+        <Container style={{height:"60vh"}}>
+          <h1>Fluid jumbotron</h1>
+          <p>
+            This is a modified jumbotron that occupies the entire horizontal space of
+            its parent.
+          </p>
+        </Container>
+      </Jumbotron>
+
+      {/* Fourth jumbotron */}
+      <Jumbotron fluid>
+        <Container style={{height:"90vh"}}>
+          <h1>Fluid jumbotron</h1>
+          <p>
+            This is a modified jumbotron that occupies the entire horizontal space of
+            its parent.
+          </p>
+        </Container>
+      </Jumbotron>
+
+      {/* Fifth jumbotron */}
+      <Jumbotron fluid>
+        <Container style={{height:"50vh"}}>
+          <h1>Fluid jumbotron</h1>
+          <p>
+            This is a modified jumbotron that occupies the entire horizontal space of
+            its parent.
+          </p>
+        </Container>
+      </Jumbotron>
+
+      {/* Footer */}
+      <Navbar expand="lg" bg="none" variant="light" className="p-5" sticky="bottom">
+                <Nav className="ml-auto mr-auto">
+                    <Row>
+                        <Col className="text-left mr-5 pr-5">
+                            <Navbar.Brand className="ml-auto mr-auto"href="#home">Storefront</Navbar.Brand>
+                            <Navbar.Text className="">Address</Navbar.Text>
+                        </Col>
+
+                        <Col className="text-right ml-5 pl-5">
+                            <Nav.Link className="" href="#">Back to Top</Nav.Link>
+                            <Nav.Link className="" href="//">Contact</Nav.Link>
+                        </Col>
+                    </Row>
+                </Nav>
+        </Navbar>
+    </div>
   );
 }
 
