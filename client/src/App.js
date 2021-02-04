10c7b092c46139185881809bde7ecfb3c0a8862a
DJ Hersh <DanielJHersh@gmail.com>
2021-02-03 16:54:45 -0800

    Added input components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import CartItem from "./components/CartItem";
+import EmailPassword from "./components/EmailPassword";
+import NameInput from "./components/NameInput";
 import Cart from "./pages/cart/Cart";
 import StoreLanding from "./pages/storeLanding/StoreLanding";
 
@@ -8,7 +10,7 @@ import StoreLanding from "./pages/storeLanding/StoreLanding";
 function App() {
   return (
     <div> 
-      <Cart />
+      <NameInput />
     </div>
   );
 }
