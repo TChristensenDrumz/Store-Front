ec32e05c0c9c1c3765bb6680c8346edf0cf9a76e
DJ Hersh <DanielJHersh@gmail.com>
2021-02-03 16:32:00 -0800

    Created site landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import CartItem from "./components/CartItem";
 import Cart from "./pages/cart/Cart";
+import StoreLanding from "./pages/storeLanding/StoreLanding";
 
 
 
 function App() {
   return (
     <div> 
-      <Cart/>
+      <StoreLanding />
     </div>
   );
 }
--- client/src/components/Preview.js
+++ client/src/components/Preview.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function Preview() {
   return (
-    <div className="d-flex justify-content-center">
+    <div className="d-flex justify-content-center m-5">
       <Card style={{ width: "15vw" }}>
         <Image variant="top" src="https://placehold.it/600x600" fluid />
         <Card.Body>
